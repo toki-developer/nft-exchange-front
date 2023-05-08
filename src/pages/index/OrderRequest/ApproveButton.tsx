df8aec6a6968771193aba0ae0c9958e3ad4af849
Toki <55911995+tokio-k@users.noreply.github.com>
2023-05-08 09:07:08 +0900

    refactor: useTransactionの活用

--- src/pages/index/OrderRequest/ApproveButton.tsx
+++ src/pages/index/OrderRequest/ApproveButton.tsx
@@ -1,10 +1,10 @@
 import type { ButtonHTMLAttributes } from "react";
-import { useEffect } from "react";
 import {
   useNFTContract,
   useNFTExchangeContractAddress,
 } from "src/utils/contract";
 import { useWriteApprove } from "src/utils/contract";
+import { useTransaction } from "wagmi";
 
 import { STATUS, useStatusContext } from "./StatusContext";
 import { useFormValue } from "./useFormValue";
@@ -23,7 +23,7 @@ export const ApproveButton = () => {
 
 const ApproveButtonImpl = () => {
   const { senderNFTContractAddress, senderNFTTokenId } = useFormValue();
-  const { data, status, write } = useWriteApprove(
+  const { data, write } = useWriteApprove(
     senderNFTContractAddress,
     senderNFTTokenId
   );
@@ -33,37 +33,24 @@ const ApproveButtonImpl = () => {
 
   const handleApproveMyNFT = () => write?.();
 
-  //TODO: statusがsuccessじゃない時,dataがない時について調べる
-  useEffect(() => {
-    if (
-      status == "success" &&
-      inputStatus.status === STATUS.NO_APPROVED &&
-      data
-    ) {
-      (async () => {
-        const tx = await data.wait();
-        if (tx.status == 1) {
-          contract?.getApproved(senderNFTTokenId).then((res) => {
-            if (res === NFT_EXCHANGE_ADDRESS) {
-              inputStatus.setStatus(STATUS.APPROVED);
-            } else {
-              //TODO: 成功しているのに、approve先が違う場合
-              inputStatus.setStatus(STATUS.NO_APPROVED);
-            }
-          });
-        } else {
-          //TODO: approve失敗の処理
-        }
-      })();
-    }
-  }, [
-    status,
-    NFT_EXCHANGE_ADDRESS,
-    inputStatus,
-    senderNFTTokenId,
-    contract,
-    data,
-  ]);
+  useTransaction({
+    hash: data?.hash,
+    onSuccess: async (tx) => {
+      const res = await tx.wait();
+      if (res.status == 1) {
+        contract?.getApproved(senderNFTTokenId).then((res) => {
+          if (res === NFT_EXCHANGE_ADDRESS) {
+            inputStatus.setStatus(STATUS.APPROVED);
+          } else {
+            //TODO: 成功しているのに、approve先が違う場合
+            inputStatus.setStatus(STATUS.NO_APPROVED);
+          }
+        });
+      } else {
+        //TODO: エラー時の処理
+      }
+    },
+  });
 
   return <Button onClick={handleApproveMyNFT} />;
 };
